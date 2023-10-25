0d5f82ea564ba1d2840bb43eed054a3b18d80072
Shamil-G <s.gusseynov@gmail.com>
2023-10-25 11:58:10 +0600

    first commit

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# ChatGPT" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Shamil-G/ChatGPT.git
+git push -u origin main
